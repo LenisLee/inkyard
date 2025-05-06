635f75629878085c9981aa879636eac4716c0818
litie <3069903249@qq.com>
2025-05-06 15:15:02 +0800

    feat:集成mdx功能

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
+import mdx from '@astrojs/mdx'
+import {onRequest} from "./src/middleware.js";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [mdx()],
+    middleware: [onRequest]
+});
