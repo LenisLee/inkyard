27b894ad1ab85ac2ba2e1d0b2597cf8eb2ca0a45
litie <3069903249@qq.com>
2025-05-28 09:34:06 +0800

    feat:新增tailwind插件

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,14 @@ import {defineConfig} from 'astro/config';
 import mdx from '@astrojs/mdx'
 import {onRequest} from "./src/middleware.js";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [mdx()],
-    middleware: [onRequest]
+  integrations: [mdx()],
+  middleware: [onRequest],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
